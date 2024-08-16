c91454194f02fa8f9b0e4349467a1bbc648a1e4a
Suryananda07 <nandasurya500@gmail.com>
2024-08-16 23:17:51 +0800

    button warna

--- script.js
+++ script.js
@@ -1,99 +1,121 @@
-
 // BMI Start
 function limitInputLength(element, maxLength) {
-    element.addEventListener('input', function() {
-        // Hanya membatasi angka jika lebih dari maxLength
-        let value = this.value.replace(/\D/g, ''); // Menghapus karakter non-digit
-        if (value.length > maxLength) {
-            value = value.slice(0, maxLength); // Mengambil hanya maxLength karakter
-        }
-        this.value = value;
-    });
+  element.addEventListener("input", function () {
+    // Hanya membatasi angka jika lebih dari maxLength
+    let value = this.value.replace(/\D/g, ""); // Menghapus karakter non-digit
+    if (value.length > maxLength) {
+      value = value.slice(0, maxLength); // Mengambil hanya maxLength karakter
+    }
+    this.value = value;
+  });
 }
 
-        function calculateBMI() {
-            // Mendapatkan nilai input dari form
-            const weight = parseFloat(document.getElementById('weightInput').value);
-            const height = parseFloat(document.getElementById('heightInput').value);
+function calculateBMI() {
+  // Mendapatkan nilai input dari form
+  const weight = parseFloat(document.getElementById("weightInput").value);
+  const height = parseFloat(document.getElementById("heightInput").value);
 
-            // Validasi input
-            if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-                alert("Mohon masukkan nilai berat dan tinggi yang valid.");
-                return;
-            }
+  // Validasi input
+  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+    alert("Mohon masukkan nilai berat dan tinggi yang valid.");
+    return;
+  }
 
-            // Menghitung BMI
-            const heightInMeters = height / 100; // Mengubah tinggi dari cm ke meter
-            const bmi = weight / (heightInMeters * heightInMeters);
+  // Menghitung BMI
+  const heightInMeters = height / 100; // Mengubah tinggi dari cm ke meter
+  const bmi = weight / (heightInMeters * heightInMeters);
 
-            // Menampilkan hasil
-            document.getElementById('bmi').textContent = bmi.toFixed(2);
-            
-            // Menentukan kategori BMI dan saran
-            let description = '';
-            let suggestion = '';
-            if (bmi < 18.5) {
-                description = 'Kurus';
-                suggestion = 'Perbanyak Olahraga';
-            } else if (bmi < 24.9) {
-                description = 'Normal';
-                suggestion = 'Terus Pertahankan Pola Hidup Sehat';
-            } else if (bmi < 29.9) {
-                description = 'Gemuk';
-                suggestion = 'Pertimbangkan Mengurangi Berat Badan';
-            } else {
-                description = 'Obesitas';
-                suggestion = 'Konsultasikan dengan Dokter';
-            }
-            document.getElementById('desc').textContent = description;
-            document.querySelector('.bmi_saran').textContent = suggestion;
-            
-            // Menampilkan elemen hasil
-            document.querySelector('.bmi_hasil').classList.remove('hidden');
+  // Menampilkan hasil
+  document.getElementById("bmi").textContent = bmi.toFixed(2);
 
-           
-            // Membatasi input hanya 3 digit
-            const heightInput = document.getElementById('heightInput');
-            const weightInput = document.getElementById('weightInput');
-    
-            limitInputLength(heightInput, 3); 
-            limitInputLength(weightInput, 3); 
-        }
+  // Menentukan kategori BMI dan saran
+  let description = "";
+  let suggestion = "";
+  if (bmi < 18.5) {
+    description = "Kurus";
+    suggestion = "Perbanyak Olahraga";
+  } else if (bmi < 24.9) {
+    description = "Normal";
+    suggestion = "Terus Pertahankan Pola Hidup Sehat";
+  } else if (bmi < 29.9) {
+    description = "Gemuk";
+    suggestion = "Pertimbangkan Mengurangi Berat Badan";
+  } else {
+    description = "Obesitas";
+    suggestion = "Konsultasikan dengan Dokter";
+  }
+  document.getElementById("desc").textContent = description;
+  document.querySelector(".bmi_saran").textContent = suggestion;
 
-        // BMI End
+  // Menampilkan elemen hasil
+  document.querySelector(".bmi_hasil").classList.remove("hidden");
 
-        // Loader Start
-        document.addEventListener("DOMContentLoaded", function() {
-            const loaderContainer = document.querySelector('.loader-container');
-            const loaderVideo = loaderContainer.querySelector('video');
-        
-            loaderVideo.play();
-        
-            loaderVideo.onended = function() {
-                document.body.classList.add('loaded');
-            };
-        
-            window.addEventListener('load', function() {
-                setTimeout(function() {
-                    document.body.classList.add('loaded');
-                }, 6500); 
-            });
-        });
+  // Membatasi input hanya 3 digit
+  const heightInput = document.getElementById("heightInput");
+  const weightInput = document.getElementById("weightInput");
 
-        // Loader End
+  limitInputLength(heightInput, 3);
+  limitInputLength(weightInput, 3);
+}
 
-        // artikel start
-        function toggleSelengkapnya() {
-            var selengkapnyaDiv = document.querySelector('.selengkapnya');
-            var button = document.getElementById('read-more');
-        
-            if (selengkapnyaDiv.style.display === "none" || selengkapnyaDiv.style.display === "") {
-                selengkapnyaDiv.style.display = "grid"; 
-                button.innerText = "Tampilkan Lebih Sedikit"; 
-            } else {
-                selengkapnyaDiv.style.display = "none"; 
-                button.innerText = "Baca Selengkapnya"; 
-            }
-        }
+// BMI End
 
-        // artikel end
+// Loader Start
+document.addEventListener("DOMContentLoaded", function () {
+  const loaderContainer = document.querySelector(".loader-container");
+  const loaderVideo = loaderContainer.querySelector("video");
+
+  loaderVideo.play();
+
+  loaderVideo.onended = function () {
+    document.body.classList.add("loaded");
+  };
+
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      document.body.classList.add("loaded");
+    }, 6500);
+  });
+});
+
+// Loader End
+
+// artikel start
+function toggleSelengkapnya() {
+  var selengkapnyaDiv = document.querySelector(".selengkapnya");
+  var button = document.getElementById("read-more");
+
+  if (
+    selengkapnyaDiv.style.display === "none" ||
+    selengkapnyaDiv.style.display === ""
+  ) {
+    selengkapnyaDiv.style.display = "grid";
+    button.innerText = "Tampilkan Lebih Sedikit";
+  } else {
+    selengkapnyaDiv.style.display = "none";
+    button.innerText = "Baca Selengkapnya";
+  }
+}
+
+// artikel end
+
+// icon di klik start
+document.addEventListener("DOMContentLoaded", function () {
+  // Ambil elemen tombol
+  const btnPria = document.querySelector("#genderPria button");
+  const btnWanita = document.querySelector(".gender_wanita button");
+
+  // Fungsi untuk mengaktifkan tombol Pria dan menonaktifkan tombol Wanita
+  btnPria.addEventListener("click", function () {
+    btnPria.classList.add("active");
+    btnWanita.classList.remove("active");
+  });
+
+  // Fungsi untuk mengaktifkan tombol Wanita dan menonaktifkan tombol Pria
+  btnWanita.addEventListener("click", function () {
+    btnWanita.classList.add("active");
+    btnPria.classList.remove("active");
+  });
+});
+
+// icon di klik end
